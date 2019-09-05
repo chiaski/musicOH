a5f34544bcb984ce12591db7c92655cf06d96d5d
Chia <chiamisola@gmail.com>
2019-09-05 19:27:44 -0400

    bro how do i authenticate

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # musicOH
 Trying to make a better topsters  + spotify thing / OH!
+
+musicOH!/0.1 +http://chiask.com/music
--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-/ Fetch data test
+/* Fetch Data Test */
 
-fetch('https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=2fd3bf50e7a74d91d84e9a0a816153f2&artist=cher&track=believe&format=json')
+console.log("app.js is loaded");
+
+/*
+fetch('https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=thaXuVdDamQySCOQEPHV&artist=cher&track=believe&format=json')
     .then(function (response) {
         return response.json();
     })
     .then(function (myJson) {
         console.log(JSON.stringify(myJson));
     });
+
+
+$.ajax({
+    url: 'https://api.wit.ai/message?v=20140826&q=',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer 6QXNMEMFHNY4FJ5ELNFMP5KRW52WFXN5")
+    },
+    success: function (data) {
+        alert(data);
+        //process the JSON data etc
+    }
+})
+
+
+$.getJSON('your-api-url/validate.php?' + $(this).serialize + 'callback=?', function (data) {
+    if (data) console.log(data);
+});*/
+
+
+
+
+function getimage(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function show(response) {
+    console.log(response);
+}
