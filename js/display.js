366f2739e011f8b4ebfb3db3eea9a842a8afdd1a
Chia <chiamisola@gmail.com>
2019-09-05 23:46:01 -0400

    Get albums to display

--- js/display.js
+++ js/display.js
@@ -9,9 +9,11 @@ function convert(string) {
     return string.replace(/ /g, "+");
 }
 
-artistSearch = "The Hotelier";
+artistSearch = "Snail Mail";
 artistSearch = convert(artistSearch);
 
+
+testFetch(artistSearch);
 
 function testFetch(artist) {
     fetch('http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + artist + '&api_key=602cdfee63f450d681a00c86afca33c5&format=json')
@@ -20,28 +22,38 @@ function testFetch(artist) {
 
         })
         .then(function (myJson) {
-            console.log(myJson.topalbums)
+            console.log(myJson.topalbums.album)
+
+            displayAlbums(myJson.topalbums);
+
             artistQuery = JSON.stringify(myJson.topalbums.album);
-            console.log("Artist Query:" + artistQuery);
+            //console.log("Artist Query:" + artistQuery);
         });
 }
 
 
 
-console.log("Artist Query:" + artistQuery);
+// console.log("Artist Query:" + artistQuery);
 
-var artistQuery = testFetch(artistSearch);
-displayAlbums();
+// var artistQuery = testFetch(artistSearch);
 
 
-function displayAlbums() {
-    for (var i = 0; i < artistQuery.length; i++) {
+function displayAlbums(artistQuery) {
+    console.log('running!');
+    console.log(artistQuery);
+    for (var i = 0; i < artistQuery.album.length; i++) {
 
+        console.log(i);
         var albumContent;
 
-        albumContent = "<div class='album-type'><img src='" + artistQuery.album[i].image[2]['#text'] + "'></div>";
 
-        $(".album-list").append(albumContent);
+        albumContent = "<div class='album-type'><img src='" + artistQuery.album[i].image[2]['#text'] + "'></div>";
+        console.log(albumContent);
+
+        if (artistQuery.album[i].image[2]['#text'].length !== 0) {
+            $(".album-list").append(albumContent);
+            console.log(i);
+        }
 
     }
 }
