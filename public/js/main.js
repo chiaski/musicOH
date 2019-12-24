117d6f0d1a957068ff85635e22d4d265c94002c4
Chia <chiamisola@gmail.com>
2019-12-23 20:16:53 -0500

    Prettify jquery i think

--- public/js/main.js
+++ public/js/main.js
@@ -6,32 +6,52 @@ $(document).on("click", ".album-list img", function () {
     var album = $(this).attr('src'); // Gets image source of selected thing
     var albuminfo = $(this).data('artist') + ' ' + $(this).data('album');
 
-    
-    // first check if there is an empty album
-    
-    if( $(".album-box[class*='alb-empty']").first()[0] ){
-    
-    let albTitle = $(this).data('album');
-    
-    /* watch out for fiona apple
-    but also fix this cause the cutoff is weird rn
-    */
-    if(albTitle.length > 40){
-        albTitle = albTitle.substr(0, 40) + "...";
-    }
-    
-        
-        
-    $(".album-box[class*='alb-empty']").first().removeClass('alb-empty').css('background-image', 'url(' + album + ')').html("<p>" + $(this).data('artist') + "&mdash;" + albTitle + "</p>").addClass("alb-item").attr("data-artist", $(this).data('artist')).attr("data-album", $(this).data('album'));
-    
-    
-    let title = "<div class='album-title' data-artist='" + $(this).data('artist') + "'  data-album='" + $(this).data('album') + "'><strong>" + $(this).data('artist')  + "</strong>&mdash;" + albTitle + "</div>";
-    
-    
 
-    $(".album-titles").append(title);
-                
-        
+    // first check if there is an empty album
+
+    if ($(".album-box[class*='alb-empty']").first()[0]) {
+
+        let albTitle = $(this).data('album');
+
+        /* watch out for fiona apple
+        but also fix this cause the cutoff is weird rn
+        */
+        if (albTitle.length > 40) {
+            albTitle = albTitle.substr(0, 40) + "...";
+        }
+
+        // check whether we should display titles
+        if (($("button").attr("data-id", "display")).attr("data-display") == "yes") {
+
+            $(".album-box[class*='alb-empty']").first()
+                .removeClass('alb-empty')
+                .addClass("alb-item")
+                .css('background-image', 'url(' + album + ')')
+                .html("<p style='display:block;'>" + $(this).data('artist') + "&mdash;" + albTitle + "</p>")
+                .attr("data-artist", $(this).data('artist'))
+                .attr("data-album", $(this).data('album'));
+
+        } else {
+
+            $(".album-box[class*='alb-empty']").first()
+                .removeClass('alb-empty')
+                .addClass("alb-item")
+                .css('background-image', 'url(' + album + ')')
+                .html("<p>" + $(this).data('artist') + "&mdash;" + albTitle + "</p>")
+                .attr("data-artist", $(this).data('artist'))
+                .attr("data-album", $(this).data('album'));
+
+        }
+
+
+
+        let title = "<div class='album-title' data-artist='" + $(this).data('artist') + "'  data-album='" + $(this).data('album') + "'><strong>" + $(this).data('artist') + "</strong>&mdash;" + albTitle + "</div>";
+
+
+
+        $(".album-titles").append(title);
+
+
     }
 
 
@@ -40,31 +60,37 @@ $(document).on("click", ".album-list img", function () {
 // Click to remove
 
 $(document).on("click", ".alb-item", function () {
-    
-    
+
+
     let artTitle = $(this).attr('data-artist');
     let albTitle = $(this).attr('data-album');
-    
+
     console.log(artTitle + albTitle);
-    
+
     if (window.outerWidth < 500) {
         $(this).css('background-image', '').html('').removeClass('alb-item').addClass('alb-empty');
     }
-    
-    $(this).css('background-image', '').html('').removeClass('alb-item').addClass('alb-empty');
-    
-    console.log($(".album-titles").find("div[data-artist='" + artTitle + "'][data-album='" + albTitle + "']").first()[0]);
-    
-    $(".album-titles").find("[data-artist='" + artTitle + "'][data-album='" + albTitle + "']")[0].remove();
-    
-    
+
+    $(this)
+        .css('background-image', '')
+        .html('')
+        .removeClass('alb-item')
+        .addClass('alb-empty');
+
+
+    // delete first copy on album list
+    $(".album-titles")
+        .find("[data-artist='" + artTitle + "'][data-album='" + albTitle + "']")[0]
+        .remove();
+
+
 });
 
 
 // Save
 
 
-var saveImage = function() {
+var saveImage = function () {
     domtoimage.toBlob(document.getElementById('collage'))
         .then(function (blob) {
             window.saveAs(blob, 'collageOH.png');
@@ -85,50 +111,60 @@ var saveImage = function() {
 
 // Resize options
 
-var optionResize = function(method){
-    
-    switch(method){
-        case "3x3": 
+var optionResize = function (method) {
+
+    switch (method) {
+        case "3x3":
             $(".collage-wrapper").html(" "); // reset
-            for(let i=0;i<9;i++){
-                $(".collage-wrapper").append('<div class="album-box alb-empty"></div>');
-            }
-            
-            break;
-        
-            
-        case "5x5": 
-            $(".collage-wrapper").html(" "); // reset
-            for(let i=0;i<25;i++){
-                $(".collage-wrapper").append('<div class="album-box alb-empty alb-size-two"></div>');
-            }
-            
-            break;
-            
-               
-        case "display": 
-            
-            if( $(".album-box p").css("display") == "none" ){
+            for (let i = 0; i < 9; i++) {
                 
+                $(".collage-wrapper")
+                    .append('<div class="album-box alb-empty"></div>');
+            }
+
+            break;
+
+
+        case "5x5":
+            $(".collage-wrapper").html(" "); // reset
+            for (let i = 0; i < 25; i++) {
+                
+                $(".collage-wrapper")
+                    .append('<div class="album-box alb-empty alb-size-two"></div>');
+            }
+
+            break;
+
+
+        case "display":
+
+            if ($(".album-box p").css("display") == "none") {
+
                 // fade in existing
-                $(".album-box p").fadeIn("slow");
-                $(".album-box p").addClass("album-display");
+                $(".album-box p")
+                    .fadeIn("slow");
+
+                ($("button")
+                 .attr("data-id", "display"))
+                    .attr("data-display", "yes");
+
+            } else {
+
+                $(".album-box p")
+                    .fadeOut("slow");
                 
-                
-            } else{
-                
-                 $(".album-box p").fadeOut("slow");
-                $("album-box p").css("display", "none");
-                $(".album-box p").removeClass("album-display");
-                
+                $("album-box p")
+                    .css("display", "none");
+
+                ($("button").attr("data-id", "display"))
+                    .attr("data-display", "no");
             }
-            
+
             //$(".album-box p").css("background", "yellow");
-            
-            $(".collage-wrapper").addClass('wordDisplay')
+
             break;
     }
-                
+
 }
 
 
@@ -159,26 +195,24 @@ $(".input-finder").keypress(function (event) {
 });
 
 
-$('.btn-generateAlbums').click(function(){
+$('.btn-generateAlbums').click(function () {
     displayQuery.render();
 });
 
 
-$('.options-area *[data-id="3x3"]').click(function(){
+$('.options-area *[data-id="3x3"]').click(function () {
     optionResize("3x3");
 });
 
-$('.options-area *[data-id="5x5"]').click(function(){
+$('.options-area *[data-id="5x5"]').click(function () {
     optionResize("5x5");
 });
 
-$('.options-area *[data-id="display"]').click(function(){
+$('.options-area *[data-id="display"]').click(function () {
     optionResize("display");
 });
 
 
-$('.btn-saveimage').click(function(){
+$('.btn-saveimage').click(function () {
     saveImage();
 });
-
-
